7d8fe69ed183ec0f31b93e9a47b7cedc001067a0
sanji-bots-963 <uwvohrc155@nezahuys.xyz>
2024-08-01 12:59:35 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -34,7 +34,7 @@ async def start_command(client: Client, message):
                     "Jᴏɪɴ Cʜᴀɴɴᴇʟ 1", url=client.link_one),
                         InlineKeyboardButton(
                             "Jᴏɪɴ Cʜᴀɴɴᴇʟ 2", url=client.link_two)
-                          ],[ InlineKeyboardButton(" Jᴏɪɴ Cʜᴀɴɴᴇʟ 3 ", url="https://t.me/+Euuw_DXNxhxkMjFl")]
+                          ],[ InlineKeyboardButton(" Jᴏɪɴ Cʜᴀɴɴᴇʟ 3 ", url="https://t.me/+e4Et4XgMhMBiMzM1")]
             ]
             try:
                 btn.append(
